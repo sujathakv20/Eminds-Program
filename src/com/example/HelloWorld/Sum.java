84fafd02adb8805a993977ad247bba78df24ea34
Sujatha <vsujathak@gmail.com>
2022-11-16 10:16:47 +0530

    Java programs

---
+++ src/com/example/HelloWorld/Sum.java
@@ -0,0 +1,15 @@
+package com.example.HelloWorld;
+
+public class Sum
+{
+    public static void main(String[] args) {
+        int i=3;
+        int j=3;
+        int k;
+        k=j+i;
+
+    {
+        System.out.println(k);
+    }
+    }
+}
